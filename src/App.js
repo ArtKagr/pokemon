793e32cff45d6ca27a2c9ec0f0d566ba0e930f23
Artem <kagramanov.kld@yandex.ru>
2019-11-08 22:25:22 +0300

    seventh commit

--- src/App.js
+++ src/App.js
@@ -4,45 +4,52 @@ import './App.css';
 export default class App extends Component {
   constructor(props){
     super(props)
-    this.state = {newUrl: [], newSymbol: [], code: [], releaseDate: []};
+    this.state = {code: []};
   }
     
 componentDidMount = async() => {
   const responce = await fetch('https://api.pokemontcg.io/v1/sets');
   const data = await responce.json();
-  
-  let logestUrl = [];
-  let symbestUrl = [];
-  let arrKey = [];
-  let releaseDate = []
   console.log(data.sets);
+  let arrKey = [];
+  this.standardLegal = <li></li>;
+  this.expandedLegal = <li></li>;
+
     for(let i = 0; i < data.sets.length; i++) {
-        logestUrl.push(<img src={data.sets[i].logoUrl} alt='logo' className="set__image"></img>);
-        symbestUrl.push(<img src={data.sets[i].symbolUrl} alt='symbol' className="set__content-image"></img>);
-        arrKey.push(<div key={data.sets[i].code} className="set"></div>);
-        releaseDate.push(data.sets[i].releaseDate);
+
+        if(data.sets[i].standardLegal === true){
+          this.standardLegal = <li>StandardLegal</li>;
+        }
+        if(data.sets[i].expandedLegal === true){
+          this.expandedLegal = <li>ExpandedLegal</li>;
+        }
+
+        arrKey.push(<div key={data.sets[i].code} className="set">
+          <img src={data.sets[i].logoUrl} alt='logo' className="set__image"></img>
+          <div className="set__content">
+          <img src={data.sets[i].symbolUrl} alt='symbol' className="set__content-image"></img>
+            <div className="set__content-container">
+              <h1 className="set__content-title">{data.sets[i].name}</h1>
+              <h2 className="set__content-subtitle">Released {data.sets[i].releaseDate}</h2>
+            </div>
+              <ul className="set__content-description">
+                <li>
+                  {this.standardLegal}
+                </li>
+                <li>
+                  {this.expandedLegal}
+                </li>
+              </ul>
+            </div>
+        </div>);
     }
 
-  this.setState({newUrl: logestUrl, newSymbol: symbestUrl, code: arrKey, releaseDate: releaseDate})
+  this.setState({code: arrKey})
 }
 
     render() {
-        console.log(this.state);
-        return (
-        <div>{this.state.code}</div>
-            /*<div key='{this.state.code[0]}' className="set">     
-                <img  src='{this.state.newUrl[0]}' alt='logo' className="set__image"></img>
-            <div className="set__content">
-                <img src='{this.state.newSymbol[0]}' alt='symbol' className="set__content-image"></img>
-            <div className="set__content-container">
-    
-            </div>
-            <ul className="set__content-description">
-    
-            </ul>
-    
-            </div>
-            </div>*/
-        )   
+          return (
+            <div className="set-list">{this.state.code}</div>
+        )
     }
 }
