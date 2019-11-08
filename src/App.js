596b53095185b83c295c021928df7bf2aa788e28
Artem <kagramanov.kld@yandex.ru>
2019-11-08 19:32:52 +0300

    fifth commit

--- src/App.js
+++ src/App.js
@@ -4,31 +4,36 @@ import './App.css';
 export default class App extends Component {
   constructor(props){
     super(props)
-    this.state = {newUrl: [], newSymbol: []};
+    this.state = {newUrl: [], newSymbol: [], code: [], releaseDate: []};
   }
     
 componentDidMount = async() => {
   const responce = await fetch('https://api.pokemontcg.io/v1/sets');
   const data = await responce.json();
-
+  
   let logestUrl = [];
   let symbestUrl = [];
-  
+  let arrKey = [];
+  let releaseDate = []
+  console.log(data.sets);
     for(let i = 0; i < data.sets.length; i++) {
-        logestUrl.push(data.sets[i].logoUrl);
-        symbestUrl.push(data.sets[i].symbolUrl);
+        logestUrl.push(<img src={data.sets[i].logoUrl} alt='logo' className="set__image"></img>);
+        symbestUrl.push(<img src={data.sets[i].symbolUrl} alt='symbol' className="set__content-image"></img>);
+        arrKey.push(<div key={data.sets[i].code} className="set"></div>);
+        releaseDate.push(data.sets[i].releaseDate);
     }
 
-  this.setState({newUrl: logestUrl, newSymbol: symbestUrl})
+  this.setState({newUrl: logestUrl, newSymbol: symbestUrl, code: arrKey, releaseDate: releaseDate})
 }
 
     render() {
         console.log(this.state);
         return (
-            <div className="set">     
-                <img src={this.state.newUrl[0]} alt='logo' className="set__image"></img>
+        <div>{this.state.code}</div>
+            /*<div key='{this.state.code[0]}' className="set">     
+                <img  src='{this.state.newUrl[0]}' alt='logo' className="set__image"></img>
             <div className="set__content">
-                <img src={this.state.newSymbol[0]} alt='symbol' className="set__content-image"></img>
+                <img src='{this.state.newSymbol[0]}' alt='symbol' className="set__content-image"></img>
             <div className="set__content-container">
     
             </div>
@@ -37,7 +42,7 @@ componentDidMount = async() => {
             </ul>
     
             </div>
-            </div>
+            </div>*/
         )   
     }
 }
